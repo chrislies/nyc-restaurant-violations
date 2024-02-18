d32bde38b60eda7884f53e3039bc1b6609920277
chrislies <clai20707@gmail.com>
2024-02-17 19:52:50 -0500

    improved cluster dispersion

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,15 @@ import Feature from "ol/Feature";
 import Point from "ol/geom/Point";
 import { Vector as VectorLayer } from "ol/layer";
 import { Vector as VectorSource } from "ol/source";
-import { Style, Icon, Circle as CircleStyle, Stroke, Fill, Text } from "ol/style";
+import {
+  Style,
+  Icon,
+  Circle as CircleStyle,
+  Stroke,
+  Fill,
+  Text,
+} from "ol/style";
 import { Cluster } from "ol/source";
-
 
 const MarkerSvg =
   "data:image/svg+xml;charset=utf-8,%3C%3Fxml version%3D%221.0%22%20%3F%3E%3Csvg%20height%3D%2224%22%20version%3D%221.1%22%20width%3D%2224%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Acc%3D%22http%3A%2F%2Fcreativecommons.org%2Fns%23%22%20xmlns%3Adc%3D%22http%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%22%20xmlns%3Ardf%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%22%3E%3Cg%20transform%3D%22translate(0%20-1028.4)%22%3E%3Cpath%20d%3D%22m12%200c-4.4183%202.3685e-15%20-8%203.5817-8%208%200%201.421%200.3816%202.75%201.0312%203.906%200.1079%200.192%200.221%200.381%200.3438%200.563l6.625%2011.531%206.625-11.531c0.102-0.151%200.19-0.311%200.281-0.469l0.063-0.094c0.649-1.156%201.031-2.485%201.031-3.906%200-4.4183-3.582-8-8-8zm0%204c2.209%200%204%201.7909%204%204%200%202.209-1.791%204-4%204-2.2091%200-4-1.791-4-4%200-2.2091%201.7909-4%204-4z%22%20fill%3D%22%23e74c3c%22%20transform%3D%22translate(0%201028.4)%22%2F%3E%3Cpath%20d%3D%22m12%203c-2.7614%200-5%202.2386-5%205%200%202.761%202.2386%205%205%205%202.761%200%205-2.239%205-5%200-2.7614-2.239-5-5-5zm0%202c1.657%200%203%201.3431%203%203s-1.343%203-3%203-3-1.3431-3-3%201.343-3%203-3z%22%20fill%3D%22%23c0392b%22%20transform%3D%22translate(0%201028.4)%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E";
@@ -88,7 +94,6 @@ export default function Home() {
 
           if (!isNaN(latitude) && !isNaN(longitude)) {
             const coordinateKey = `${item.camis},${latitude},${longitude}`;
-            // const camisKey = item.camis;
 
             if (!addedCoordinates.current.has(coordinateKey)) {
               const marker = new Feature({
@@ -102,46 +107,53 @@ export default function Home() {
 
         // Create a cluster source
         const clusterSource = new Cluster({
-          distance: 50,
+          distance: calculateClusterDistance(map.getView().getZoom() ?? 0),
           source: vectorSource,
         });
 
-     // Create a vector layer with the cluster source
-     const clusterLayer = new VectorLayer({
-      source: clusterSource,
-      style: (feature) => {
-        const size = feature.get("features").length;
-        if (size === 1) {
-          // Individual marker
-          return new Style({
-            image: new Icon({
-              anchor: [0.5, 1],
-              src: MarkerSvg,
-            }),
-          });
-        } else {
-          // Cluster
-          return new Style({
-            image: new CircleStyle({
-              radius: 10,
-              fill: new Fill({
-                color: "#3399CC",
-              }),
-              stroke: new Stroke({
-                color: "#fff",
-                width: 2,
-              }),
-            }),
-            text: new Text({
-              text: size.toString(),
-              fill: new Fill({
-                color: "#fff",
-              }),
-            }),
-          });
-        }
-      },
-    });
+        // Update cluster source distance when map is zoomed
+        map.getView().on("change:resolution", () => {
+          clusterSource.setDistance(
+            calculateClusterDistance(map.getView().getZoom() ?? 0)
+          );
+        });
+
+        // Create a vector layer with the cluster source
+        const clusterLayer = new VectorLayer({
+          source: clusterSource,
+          style: (feature) => {
+            const size = feature.get("features").length;
+            if (size === 1) {
+              // Individual marker
+              return new Style({
+                image: new Icon({
+                  anchor: [0.5, 1],
+                  src: MarkerSvg,
+                }),
+              });
+            } else {
+              // Cluster
+              return new Style({
+                image: new CircleStyle({
+                  radius: 10,
+                  fill: new Fill({
+                    color: "#3399CC",
+                  }),
+                  stroke: new Stroke({
+                    color: "#fff",
+                    width: 2,
+                  }),
+                }),
+                text: new Text({
+                  text: size.toString(),
+                  fill: new Fill({
+                    color: "#fff",
+                  }),
+                }),
+              });
+            }
+          },
+        });
 
         // Add the cluster layer to the map
         map.addLayer(clusterLayer);
@@ -158,6 +170,21 @@ export default function Home() {
 
     fetchData();
   }, []);
+
+  const calculateClusterDistance = (zoom: number | undefined) => {
+    if (zoom === undefined) return 50; // Default cluster distance
+
+    switch (true) {
+      case zoom <= 10:
+        return 100;
+      case zoom <= 15:
+        return 50;
+      case zoom <= 20:
+        return 10;
+      default:
+        return 0;
+    }
+  };
 
   return (
     <>
