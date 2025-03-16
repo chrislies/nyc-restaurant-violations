769be74391ecddcbdd97234e3fb95f7b4f979474
Christopher Lai <clai20707@gmail.com>
2025-03-15 22:43:00 -0400

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "RestVT",
-  description: "Restaurant Violation Tracker",
+  title: "RestVT (Restaurant Violation Tracker)",
+  description:
+    "RestVT is a dynamic web app that displays every NYC restaurant with health and safety violations on an interactive map interface.",
 };
 
 export default function RootLayout({
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
